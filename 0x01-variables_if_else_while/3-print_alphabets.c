d0b289a686c311bd49014ee3b5c7fb57a127410c
Static1-Jem <ngugijemimah1@gmail.com>
2023-06-18 11:37:45 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lowercase;
+char uppercase;
+lowercase = 'a';
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase++;
+}
+uppercase = 'A';
+while (uppercase <= 'Z')
+{
+putchar(uppercase);
+uppercase++;
+}
+putchar('\n');
+return (0);
+}
