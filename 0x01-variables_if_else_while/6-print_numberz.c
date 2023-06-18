ce3b0a6a16070ff530cb5b135b2decbf73d94919
Static1-Jem <ngugijemimah1@gmail.com>
2023-06-18 11:56:46 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point nurmberz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+for (number = 48; number < 58; number++)
+{
+putchar(number);
+}
+putchar('\n');
+return (0);
+}
