d7dffdfa0f38229b673645e1c6abd12981943e42
Static1-Jem <ngugijemimah1@gmail.com>
2023-06-20 13:14:54 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9 times table.
+ */
+void times_table(void)
+{
+int row, column, product;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+product = row * column;
+if (column == 0)
+printf("%2d", product);
+else
+printf(", %2d", product);
+}
+printf("\n");
+}
+}
